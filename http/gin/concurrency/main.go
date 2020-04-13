667fabf7326916dc0a997d9a5cb1646a49ffdc3b
luojie <xiaoyaojie710@163.com>
2020-04-13 10:29:20 +0800

    修改注释

--- http/gin/concurrency/main.go
+++ http/gin/concurrency/main.go
@@ -14,8 +14,8 @@ var (
 //结论：无法保证安全性 虽然出现的概率并不会太高 但是一定会出现
 //误区：千万不要使用单核机器进行测试
 //google cloud：单核表现正常
-//aws ec2: 8核出panic
-//win 10: 也会painc
+//aws ec2: 8核出 panic
+//win 10: 也会 panic
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
