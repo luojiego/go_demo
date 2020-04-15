b21c16b686dcdd122484255fa652759d7d5be195
luojie <13369189710@163.com>
2020-04-16 07:52:03 +0800

    尝试 WithValue 作为 ctx 的子 Context 在 goroutines 获取 value, 并在父 Context 取消之后，子 Context 也会正常的获取消息，非常舒服。

--- context/demo/main.go
+++ context/demo/main.go
@@ -22,6 +22,20 @@ func main() {
 		}
 	}(ctx)
 
+	valCtx := context.WithValue(ctx, "key", "val monitor")
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println(ctx.Value("key"), "monitor quit, stop...")
+				return
+			default:
+				fmt.Println(ctx.Value("key"), "goroutines monitoring")
+				time.Sleep(time.Second)
+			}
+		}
+	}(valCtx)
+
 	time.Sleep(5 * time.Second)
 	fmt.Println("quit...")
 	cancel()
