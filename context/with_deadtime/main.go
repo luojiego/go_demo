0ad21f09bc8713e4eb7826a34139c8934f27e199
luojie <13369189710@163.com>
2020-04-17 07:38:19 +0800

    go context 设置了 Deadline, goroutine 自动结束

--- context/with_deadtime/main.go
+++ context/with_deadtime/main.go
@@ -6,24 +6,28 @@ import (
 	"time"
 )
 
-//黑人？？ 不太明白
+func work(ctx context.Context) error {
+	for i := 0; i < 1000; i++ {
+		select {
+		case <-time.After(1 * time.Second):
+			fmt.Println("Doing some work ", i)
+
+		case <-ctx.Done():
+			fmt.Println("Cancel the context ", i)
+			return ctx.Err()
+		}
+	}
+	return nil
+}
+
 func main() {
 	d := time.Now().Add(6 * time.Second)
-	ctx, cancel := context.WithDeadline(context.Background(), d)
+	//设置自动结束的时间
+	ctx, _ := context.WithDeadline(context.Background(), d)
 
-	// Even though ctx will be expired, it is good practice to call its
-	// cancelation function in any case. Failure to do so may keep the
-	// context and its parent alive longer than necessary.
-	defer cancel()
+	//defer cancel()
 
-	go func(ctx context.Context) {
-		select {
-		case <-time.After(2 * time.Second):
-			fmt.Println("oversleep")
-		case <-ctx.Done():
-			fmt.Println(ctx.Err())
-		}
-	}(ctx)
+	go work(ctx)
 
 	time.Sleep(10 * time.Second)
 }
