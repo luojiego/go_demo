7ddac03e0677bce70aa65f69101a6645d451d2dc
luojie <xiaoyaojie710@163.com>
2020-04-24 17:37:44 +0800

    优化测试的输出

--- unsafe/convert/string2slice/main.go
+++ unsafe/convert/string2slice/main.go
@@ -26,11 +26,11 @@ func string2bytes2(s string) []byte {
 
 func main() {
 	s1 := string2bytes1("Roger")
-	fmt.Println(s1)
-	fmt.Println(len(s1))
-	fmt.Println(cap(s1))
+	fmt.Printf("s1: %s, len:%d, cap:%d\n", string(s1), len(s1), cap(s1))
+	s1 = append(s1, " Golang"...)
+	fmt.Printf("s1: %s, len:%d, cap:%d\n", string(s1), len(s1), cap(s1))
 	s2 := string2bytes2("Roger")
-	fmt.Println(s2)
-	fmt.Println(len(s2))
-	fmt.Println(cap(s2))
+	fmt.Printf("s2: %s, len:%d, cap:%d\n", string(s2), len(s2), cap(s2))
+	s2 = append(s2, " Golang"...)
+	fmt.Printf("s2: %s, len:%d, cap:%d\n", string(s2), len(s2), cap(s2))
 }
