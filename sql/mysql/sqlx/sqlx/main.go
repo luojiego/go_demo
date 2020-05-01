07aa568108fa38dbf52423b18e38dcdfd74cb19a
luojie <13369189710@163.com>
2020-05-02 07:59:48 +0800

    原生 sqlx 尝试

---
+++ sql/mysql/sqlx/sqlx/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+	dns := "root:password@tcp(127.0.0.1:3306)/game_db"
+
+	db, err := sqlx.Connect("mysql", dns)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	count := 0
+	err = db.Get(&count, "SELECT COUNT(*) FROM acoount_data")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(count)
+}
