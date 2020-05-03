50627b949c929dc1ed31f9102b8713e835dd47aa
luojie <13361989710@163.com>
2020-05-03 09:01:54 +0800

    尝试一下 Tip of the Day

--- tools/gops/main.go
+++ tools/gops/main.go
@@ -11,4 +11,9 @@ func main() {
 		log.Fatal(err)
 	}
 	time.Sleep(time.Hour)
+	//Tip of the Day
+	//1. strings.ra -> //自动补全 ReplaceAll
+	//2. Ctrl + Shift + J // Vim 的 J 指令
+	//3. Shift + ⌘ + F7 在当前文件中高亮鼠标所处符号 ⌘ + G 向前寻找目标 Shift + ⌘ + G 向后寻找目标
+
 }
