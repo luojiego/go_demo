a45114cbf2bdd22fb826a2244d5a451325d63c9f
luojie <xiaoyaojie710@163.com>
2020-05-07 14:15:12 +0800

    修改字体大小 方便查看

--- docker/http_demo/main.go
+++ docker/http_demo/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello Roger!"))
+		writer.Write([]byte("<H1>Hello Roger!</H1>"))
 	})
 
 	http.ListenAndServe(":9090", nil)
