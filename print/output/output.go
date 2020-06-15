be46a29fc674a7db1e8c4ba3e0fce765e3290bbc
luojie <xiaoyaojie710@163.com>
2020-06-15 10:38:41 +0800

    在 Windows 上展示的日志颜色确认

---
+++ print/output/output.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	array := []int{100,200,300,400,500,600}
+	for _, v := range array {
+		fmt.Printf("%d\n", v)
+		log.Printf("%d\n", v)
+	}
+}
