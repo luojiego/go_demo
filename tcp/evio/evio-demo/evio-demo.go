ef2b2a1745d85a99558e122f3643b33a8cb70a3c
luojie <13369189710@163.com>
2020-08-21 23:12:22 +0800

    测试程序修改

--- tcp/evio/evio-demo/evio-demo.go
+++ tcp/evio/evio-demo/evio-demo.go
@@ -1,17 +1,27 @@
 package main
 
-import "github.com/tidwall/evio"
+import (
+	"github.com/tidwall/evio"
+	"log"
+)
 
 func main() {
 	var events evio.Events
 	events.Data = func(c evio.Conn, in []byte) (out []byte, action evio.Action) {
-		return nil, evio.Close
-		if string(in) == "quit" {
+
+		if string(in) == "quit\r\n" {
 			return nil, evio.Close
 		} else {
 			out = in
 		}
 		return
+	}
+	events.Opened = func(c evio.Conn) (out []byte, opts evio.Options, action evio.Action) {
+		out = []byte("欢迎来到 1024\r\n")
+		return
+	}
+	events.PreWrite = func() {
+		log.Println("what?")
 	}
 	if err := evio.Serve(events, "tcp://localhost:5000"); err != nil {
 		panic(err.Error())
