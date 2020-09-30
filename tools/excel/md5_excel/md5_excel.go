c48f616068c1973079fa3dde0bbe754b7949edb4
luojie <xiaoyaojie710@163.com>
2020-09-30 11:53:10 +0800

    优化转换程序，增加结果是否为大写的选项，同时移除原始列左右的空格

--- tools/excel/md5_excel/md5_excel.go
+++ tools/excel/md5_excel/md5_excel.go
@@ -6,20 +6,23 @@ import (
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
 	"strconv"
+	"strings"
 )
 
 var (
-	filename  = ""
-	sheetName = ""
-	srcRow    = ""
-	dstRow    = ""
+	filename    = ""
+	sheetName   = ""
+	srcRow      = ""
+	dstRow      = ""
+	upperResult = false
 )
 
 func init() {
 	flag.StringVar(&filename, "filename", "test.xlsx", "目标文件名称")
 	flag.StringVar(&sheetName, "sheet", "Sheet1", "excel 标签页[一定要确保正确]")
-	flag.StringVar(&srcRow, "src", "F", "目标列")
+	flag.StringVar(&srcRow, "src", "F", "目标列[左右的空格会被移除]")
 	flag.StringVar(&dstRow, "dst", "G", "md5 结果写入列")
+	flag.BoolVar(&upperResult, "upperResult", false, "md5 结果转成大写")
 }
 
 func main() {
@@ -35,8 +38,13 @@ func main() {
 
 	for k, row := range rows {
 		if k > 0 {
-			s := row[[]byte(srcRow)[0]-'A']
-			d := fmt.Sprintf("%x", md5.Sum([]byte(s)))
+			s := strings.TrimSpace(row[[]byte(srcRow)[0]-'A'])
+			d := ""
+			if upperResult {
+				d = fmt.Sprintf("%X", md5.Sum([]byte(s)))
+			} else {
+				d = fmt.Sprintf("%x", md5.Sum([]byte(s)))
+			}
 			file.SetCellValue(sheetName, dstRow+strconv.Itoa(k+1), d)
 			fmt.Println(s, d, dstRow+strconv.Itoa(k))
 		}
