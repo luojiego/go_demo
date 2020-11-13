c0ac5a3ad1ec47405d2a9053d5a60bf1c5d55dc0
luojie <xiaoyaojie710@163.com>
2020-11-13 17:06:34 +0800

    文件大小测试

--- file/size/main.go
+++ file/size/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bytes"
-	"encoding/binary"
 	"os"
+	"strconv"
 )
 
 type S struct {
@@ -12,17 +12,21 @@ type S struct {
 }
 
 func main() {
-	buf := new(bytes.Buffer)
-	for i := 10000000; i < 20000000; i++ {
-		// s := S{Id: strconv.Itoa(i), Value: 4200000000 + int64(i)}
-		if err := binary.Write(buf, binary.LittleEndian, 4200000000+int64(i)); err != nil {
-			panic(err)
-		}
+	// buf := new(bytes.Buffer)
+	// for i := 10000000; i < 20000000; i++ {
+	// 	// s := S{Id: strconv.Itoa(i), Value: 4200000000 + int64(i)}
+	// 	if err := binary.Write(buf, binary.LittleEndian, 4200000000+int64(i)); err != nil {
+	// 		panic(err)
+	// 	}
 
-		if err := binary.Write(buf, binary.LittleEndian, 4200000000+int64(i)); err != nil {
-			panic(err)
-		}
+	// 	if err := binary.Write(buf, binary.LittleEndian, 4200000000+int64(i)); err != nil {
+	// 		panic(err)
+	// 	}
 
+	// }
+	var buf bytes.Buffer
+	for i := 0; i < 12000000; i++ {
+		buf.WriteString(strconv.Itoa(i+10000000) + "\n")
 	}
 
 	file, err := os.Create("result.txt")
