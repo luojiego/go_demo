77912d6f428e9bfb678e7474295215b8b3017356
罗杰 <13369189710@163.com>
2021-04-13 17:58:55 +0800

    确认 ReplaceAll 的功能

--- built/string/replace/main.go
+++ built/string/replace/main.go
@@ -7,6 +7,10 @@ import (
 
 //使用 [:4] 这样的方式取得 str 的前4个字符赋值给 s，然后s使用strings的replace方法, 会不会修改原字符str
 
+const (
+	name = `luo"jie"`
+)
+
 func main() {
 	str := "ABCDEFGHIJKLMN"
 
@@ -15,5 +19,9 @@ func main() {
 	s = strings.ReplaceAll(s, "A", "a")
 	s = strings.ReplaceAll(s, "B", "b")
 
+	name1 := strings.ReplaceAll(name, "\"", "\"\"")
+
 	fmt.Println(s, str)
+
+	fmt.Printf("name: %s\n", name1)
 }
