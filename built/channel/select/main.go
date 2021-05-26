2e7c9cdd4db17308d1ba825d721af52253efe515
罗杰 <13369189710@163.com>
2021-05-26 15:08:02 +0800

    增加新的测试

--- built/channel/select/main.go
+++ built/channel/select/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func unBufferChannel() {
 	println("unBufferChannel")
 	var ch = make(chan int)
@@ -24,7 +29,34 @@ func bufferChannel() {
 	}
 }
 
+type TestData struct {
+	Name string
+	Age  int
+}
+
 func main() {
-	unBufferChannel()
-	bufferChannel()
+	var ch = make(chan *TestData, 100)
+	go func() {
+		for {
+			select {
+			case v := <-ch:
+				fmt.Println(v)
+			default:
+				// 使用 select 真的非常方便，可以检测接收的数据
+				// fmt.Println("error")
+				// 如果没有消息，则等 1s，避免对 CPU 造成过大压力
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+	// unBufferChannel()
+	// bufferChannel()
+
+	for i := 0; i < 20; i++ {
+		ch <- &TestData{
+			Name: "罗杰",
+			Age:  30 + i,
+		}
+	}
+	select {}
 }
