b86cd063a211d681e064063ea282e0dca5082439
罗杰 <13369189710@163.com>
2021-11-11 14:35:54 +0800

    sync 目录调整，errgroup 小 demo

---
+++ sync/errgroup/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	var g errgroup.Group
+	// 启动第一个子任务，它执行成功
+	g.Go(func() error {
+		time.Sleep(5 * time.Second)
+		fmt.Println("exec #1")
+		return nil
+	})
+	// 启动第二个子任务，它执行成功
+	g.Go(func() error {
+		time.Sleep(10 * time.Second)
+		fmt.Println("exec #2")
+		return errors.New("failed to exec #2")
+	})
+	g.Go(func() error {
+		time.Sleep(15 * time.Second)
+		fmt.Println("exec #3")
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Println("failed: ", err)
+		return
+	}
+	fmt.Println("Successfully exec all")
+}
