3d5692e3cae334e440d06dc208f290b9600eadd4
罗杰 <xiaoyaojie005@gmail.com>
2024-02-24 18:20:46 +0800

    单链表其他常见问题整理

--- algorithm/linklist/list/list.go
+++ algorithm/linklist/list/list.go
@@ -1,6 +1,8 @@
 package list
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type List struct {
 	Node int
@@ -29,6 +31,23 @@ func NewList(val ...int) *List {
 		node = node.Next
 	}
 
+	return head.Next
+}
+
+func NewCycleList(cycleIndex int, val ...int) *List {
+	head := &List{Node: -1, Next: nil}
+
+	node := head
+	var cycleNode *List
+	for i := 0; i < len(val); i++ {
+		node.Next = &List{Node: val[i], Next: nil}
+		node = node.Next
+		if i == cycleIndex {
+			cycleNode = node
+		}
+	}
+
+	node.Next = cycleNode
 	return head.Next
 }
 
@@ -73,4 +92,85 @@ func MergeTwoList(first, second *List) *List {
 		second.Next = MergeTwoList(first, second.Next)
 		return second
 	}
+}
+
+// 判断链表是否有环
+// 使用快慢指针，如果有环，快指针一定会追上慢指针
+func HasCycle(head *List) bool {
+	if head == nil {
+		return false
+	}
+
+	slow, fast := head, head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
+
+func HasCycleUseHash(head *List) bool {
+	h := make(map[*List]struct{})
+	for head != nil {
+		if _, ok := h[head]; ok {
+			return true
+		}
+		h[head] = struct{}{}
+		head = head.Next
+	}
+	return false
+}
+
+func ReverseLinkList(head *List) *List {
+	var pre *List
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
+
+// 递归实现
+func recur(cur, pre *List) *List {
+	if cur == nil {
+		return pre
+	}
+	res := recur(cur.Next, cur)
+	cur.Next = pre
+	return res
+}
+
+// 递归实现翻转链表
+func ReverseLinkListByRecur(head *List) *List {
+	return recur(head, nil)
+}
+
+func MiddleNode(head *List) *List {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func KThToLast(head *List, k int) *List {
+	pre, cur := head, head
+	for i := 0; i < k; i++ {
+		if cur.Next == nil {
+			return pre
+		}
+		cur = cur.Next
+	}
+	for cur != nil {
+		pre = pre.Next
+		cur = cur.Next
+	}
+	return pre
 }
--- algorithm/linklist/main.go
+++ algorithm/linklist/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	mylist "linklist/list"
 )
 
@@ -17,4 +18,19 @@ func main() {
 	second = mylist.NewList(2, 4, 6)
 
 	mylist.MergeTwoList(first, second).Print()
+
+	hasCycleList := mylist.NewCycleList(2, 6, 7, 8, 9, 10)
+	fmt.Printf("has cycle: %t\n", mylist.HasCycle(hasCycleList))
+	fmt.Printf("has cycle: %t\n", mylist.HasCycle(first))
+	fmt.Printf("has cycle use hash: %t\n", mylist.HasCycleUseHash(hasCycleList))
+	fmt.Printf("has cycle use hash: %t\n", mylist.HasCycleUseHash(first))
+
+	mylist.ReverseLinkList(mylist.NewList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)).Print()
+	mylist.ReverseLinkListByRecur(mylist.NewList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)).Print()
+
+	mylist.MiddleNode(mylist.NewList(1, 2, 3, 4, 5)).Print()
+	mylist.MiddleNode(mylist.NewList(1, 2, 3, 4, 5, 6)).Print()
+
+	mylist.KThToLast(mylist.NewList(1, 2, 3, 4, 5), 2).Print()
+	mylist.KThToLast(mylist.NewList(1, 2, 3, 4, 5, 6), 5).Print()
 }
